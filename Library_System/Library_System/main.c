6621a6d8c950526369d4656c68c64ec00a1188e3
JunHyun <grssa@naver.com>
2021-05-08 23:34:30 +0900

    도서 입력 구현중
    포인터 개념 다시 해봐야 할듯

--- Library_System/Library_System/main.c
+++ Library_System/Library_System/main.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 typedef struct book {
 	char name[20];
 	char author[30];
 	int price;
+	int bookCount;
 }BOOK;
+
+void addBook(BOOK*);
+void showBook(BOOK*);
 
 int main(void) {
 
 	int select = 0;
+	BOOK bookshelf[10];
+	BOOK* p = bookshelf;
 
 	while (select != 5) {
 		printf("도서 관리 프로그램\n");
@@ -25,7 +32,8 @@ int main(void) {
 		{
 		case 1 :
 			printf("도서 입력을 선택하셨습니다.\n");
-			
+			addBook(p);
+			showBook(p);
 			break;
 
 		case 2 :
@@ -48,7 +56,24 @@ int main(void) {
 		}
 	}
 
-
-
 	return 0;
+}
+
+void addBook(BOOK* book) {
+	printf("*** 새로운 도서를 추가합니다 ***\n");
+
+	printf("제목을 입력하세요: \n");
+	scanf("%s", book->name);
+	printf("저자를 입력하세요: \n");
+	scanf("%s", book->author);
+	printf("가격을 입력하세요: \n");
+	scanf("%d", book->price);
+
+}
+void showBook(BOOK* book) {
+	for (int i = 0; i < 10; i++) {
+		printf("제목: %s\n", book->name);
+		printf("저자: %s\n", book->author);
+		printf("가격: %d 원\n", book->price);
+	}
 }
